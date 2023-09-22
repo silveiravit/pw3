b8ecf735c18a90077113c515bcfcfc2f76740ee6
silveiravit <vitorsilveira17@outlook.com>
2023-09-21 21:39:58 -0300

    envio do projeto

---
+++ padaria/app/Http/Controllers/CadastroController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Padaria;
+
+class CadastroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+        $produto = Padaria::all();
+
+        foreach($produto as $p){
+            echo "Id ". $p->idproduto . "<br />";            
+            echo "Categoria do produto ".$p->categoriaproduto . "<br />"; 
+            echo "Produto ".$p->produto . "<br />";
+            echo "Descrição ".$p->desproduto . "<br />";             
+            echo "Valor ".$p->valor . "<br />";  
+            echo "Imagem do produto ".$p->imagemproduto . "<br />"; 
+            echo "<br />"; 
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ padaria/app/padaria.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Padaria extends Model
+{
+    // 
+    protected $table = 'tbproduto';
+}
